cae3ae6a96af390a54351d7dd6099c3c6b5b0403
ARCHIT SINGH <90107155+architsingh123@users.noreply.github.com>
2023-08-18 15:45:29 +0530

    task 1 ---> Calculator

---
+++ script.js
@@ -0,0 +1,21 @@
+let displayValue = "";
+
+function appendToDisplay(value) {
+    displayValue += value;
+    document.getElementById("result").value = displayValue;
+}
+
+function clearDisplay() {
+    displayValue = "";
+    document.getElementById("result").value = displayValue;
+}
+
+function calculate() {
+    try {
+        displayValue = eval(displayValue).toString();
+        document.getElementById("result").value = displayValue;
+    } catch (error) {
+        displayValue = "";
+        document.getElementById("result").value = "Error";
+    }
+}

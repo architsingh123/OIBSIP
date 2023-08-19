d92bd7a73a5fed8cfcbecbee295ab50521705f77
ARCHIT SINGH <90107155+architsingh123@users.noreply.github.com>
2023-08-19 10:20:48 +0530

    To Do List
    it is the task 3 of Level 2

--- script.js
+++ script.js
@@ -1,21 +1,49 @@
-let displayValue = "";
+const taskInput = document.getElementById("taskInput");
+const addTaskBtn = document.getElementById("addTaskBtn");
+const pendingTasksList = document.getElementById("pendingTasksList");
+const completedTasksList = document.getElementById("completedTasksList");
 
-function appendToDisplay(value) {
-    displayValue += value;
-    document.getElementById("result").value = displayValue;
-}
+addTaskBtn.addEventListener("click", addTask);
 
-function clearDisplay() {
-    displayValue = "";
-    document.getElementById("result").value = displayValue;
-}
+function addTask() {
+    const taskText = taskInput.value.trim();
+    if (taskText !== "") {
+        const taskItem = document.createElement("li");
+        taskItem.innerHTML = `
+            <span>${taskText}</span>
+            <span class="actions">
+                <button class="completeBtn">Complete</button>
+                <button class="editBtn">Edit</button>
+                <button class="deleteBtn">Delete</button>
+            </span>
+        `;
+        taskItem.querySelector(".completeBtn").addEventListener("click", completeTask);
+        taskItem.querySelector(".editBtn").addEventListener("click", editTask);
+        taskItem.querySelector(".deleteBtn").addEventListener("click", deleteTask);
 
-function calculate() {
-    try {
-        displayValue = eval(displayValue).toString();
-        document.getElementById("result").value = displayValue;
-    } catch (error) {
-        displayValue = "";
-        document.getElementById("result").value = "Error";
+        pendingTasksList.appendChild(taskItem);
+        taskInput.value = "";
     }
+}
+
+function completeTask(event) {
+    const taskItem = event.target.closest("li");
+    taskItem.classList.add("completed");
+    taskItem.querySelector(".completeBtn").remove();
+    taskItem.querySelector(".editBtn").remove();
+    completedTasksList.appendChild(taskItem);
+}
+
+function editTask(event) {
+    const taskItem = event.target.closest("li");
+    const taskText = taskItem.querySelector("span").textContent;
+    const newText = prompt("Edit task:", taskText);
+    if (newText !== null) {
+        taskItem.querySelector("span").textContent = newText;
+    }
+}
+
+function deleteTask(event) {
+    const taskItem = event.target.closest("li");
+    taskItem.remove();
 }

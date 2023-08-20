87125062fecda6b59a8ca1d05066f37774f34b03
ARCHIT SINGH <90107155+architsingh123@users.noreply.github.com>
2023-08-20 13:19:05 +0530

    Add files via upload
    TASK 4
    Login Authentication

--- script.js
+++ script.js
@@ -1,49 +1,22 @@
-const taskInput = document.getElementById("taskInput");
-const addTaskBtn = document.getElementById("addTaskBtn");
-const pendingTasksList = document.getElementById("pendingTasksList");
-const completedTasksList = document.getElementById("completedTasksList");
-
-addTaskBtn.addEventListener("click", addTask);
-
-function addTask() {
-    const taskText = taskInput.value.trim();
-    if (taskText !== "") {
-        const taskItem = document.createElement("li");
-        taskItem.innerHTML = `
-            <span>${taskText}</span>
-            <span class="actions">
-                <button class="completeBtn">Complete</button>
-                <button class="editBtn">Edit</button>
-                <button class="deleteBtn">Delete</button>
-            </span>
-        `;
-        taskItem.querySelector(".completeBtn").addEventListener("click", completeTask);
-        taskItem.querySelector(".editBtn").addEventListener("click", editTask);
-        taskItem.querySelector(".deleteBtn").addEventListener("click", deleteTask);
-
-        pendingTasksList.appendChild(taskItem);
-        taskInput.value = "";
+document.addEventListener('DOMContentLoaded', function() {
+    const loginForm = document.getElementById('loginForm');
+    
+    loginForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+        
+        // Simulate a network request to the backend
+        authenticateUser(username, password);
+    });
+    
+    function authenticateUser(username, password) {
+        // Simulate a successful login
+        if (username === 'user' && password === 'password') {
+            window.location.href = 'secured.html';
+        } else {
+            alert('Invalid username or password');
+        }
     }
-}
-
-function completeTask(event) {
-    const taskItem = event.target.closest("li");
-    taskItem.classList.add("completed");
-    taskItem.querySelector(".completeBtn").remove();
-    taskItem.querySelector(".editBtn").remove();
-    completedTasksList.appendChild(taskItem);
-}
-
-function editTask(event) {
-    const taskItem = event.target.closest("li");
-    const taskText = taskItem.querySelector("span").textContent;
-    const newText = prompt("Edit task:", taskText);
-    if (newText !== null) {
-        taskItem.querySelector("span").textContent = newText;
-    }
-}
-
-function deleteTask(event) {
-    const taskItem = event.target.closest("li");
-    taskItem.remove();
-}
+});
